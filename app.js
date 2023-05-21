08a1da6fc4473908676563706e90e631a6c0521a
murfadahmed <malikmurfad361@gmail.com>
2023-05-21 05:54:32 +0500

    only function

---
+++ app.js
@@ -0,0 +1,21 @@
+// let element = "upper decelerd howa hai"
+// const element = "inside" ;
+const element = "newelement"
+console.log(element)
+if(true)
+{
+    console.log(element)
+    if(true)
+    {
+        document.write(element)
+    } 
+    document.write(element)
+}
+function func (element)
+{
+    console.log(element)
+}
+func(element);
+
+// const element = "inside" ;
+// let element = "m"
